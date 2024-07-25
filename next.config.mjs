e8d5d93f6fab6ccedd408f372cbc2373fa82cb16
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-07-25 13:03:35 +0300

    i

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '***.com',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'http',
-            hostname: '',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'bit.ly',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'www.sender.net',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-      },
-};
-
-export default nextConfig;
+images: {
+remotePatterns: [
+{
+   protocol: 'https',
+   hostname: '***.com',
+   port: '',
+   pathname: '/**',
+   },
+   {
+   protocol: 'http',
+   hostname: '',
+   port: '',
+   pathname: '/**',
+   },
+   {
+   protocol: 'https',
+   hostname: 'bit.ly',
+   port: '',
+   pathname: '/**',
+   },
+   {
+   protocol: 'https',
+   hostname: 'www.sender.net',
+   port: '',
+   pathname: '/**',
+   },
+  ],
+   },
+  };
+  
+  export default nextConfig;
