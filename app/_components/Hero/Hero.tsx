a2f4128f2dd2ea782bab8efca1a8a4014ed685d3
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-07-27 11:16:01 +0300

    comment image in hero section

--- app/_components/Hero/Hero.tsx
+++ app/_components/Hero/Hero.tsx
@@ -17,9 +17,9 @@ const Hero = () => {
             <FaTwitter className="text-slate-400" />
         </Link>
       </div>
-      <Image src={`/Pineapple.png`} alt="background-image" width={1000} height={500} className="w-full h-full" />
+      {/* <Image src={`/Pineapple.png`} alt="background-image" width={1000} height={500} className="w-full h-full" /> */}
 
-      {/* <div className="section-footer bg-slate-400/55 h-[400px] rounded-3xl px-7 flex lg:flex-row flex-col justify-between items-center flex-wrap absolute bottom-0 left-0 ">
+      <div className="section-footer bg-slate-400/55 h-[400px] rounded-3xl px-7 flex lg:flex-row flex-col justify-between items-center flex-wrap  ">
         <h2 className="basis-1/2 lg:text-3xl text-sm font-bold">
           Fresh Fruit & <br /> Amazing Taist
         </h2>
@@ -30,7 +30,7 @@ const Hero = () => {
         </h4>
         <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>      
         </div>
-      </div> */}
+      </div>
     </section>
   )
 }
