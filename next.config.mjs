22db9bfc044cc5ee6f18b75c63b1697dfe9bae64
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-08-07 19:02:57 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,60 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   // images: {
-   //    domains: ['localhost','bit.ly',
-   //       'www.sender.net',
-   //       'hillfresh.eu','cdn-prod.medicalnewstoday.com','t3.ftcdn.net','www.simplyrecipes.com','hips.hearstapps.com'],
-   //  },
-images: {
-remotePatterns: [
-   {
-      protocol: 'http',
-      hostname: 'localhost',
-    },
-{
-   protocol: 'https',
-   hostname: '***.com',
-   port: '',
-   pathname: '/**',
-   },
-{
-   protocol: 'http',
-   hostname: 'localhost:4000',
-   port: '',
-   pathname: '/**',
-   },
-{
-   protocol: 'https',
-   hostname: '***.net',
-   port: '',
-   pathname: '/**',
-   },
-   {
-   protocol: 'http',
-   hostname: 'localhost',
-   port: '',
-   pathname: '/**',
-   },
-   {
-   protocol: 'https',
-   hostname: 'bit.ly',
-   port: '',
-   pathname: '/**',
-   },
-   {
-   protocol: 'https',
-   hostname: 'www.sender.net',
-   port: '',
-   pathname: '/**',
-   },
-   {
-   protocol: 'https',
-   hostname: 'hillfresh.eu',
-   port: '',
-   pathname: '/**',
-   },
-  ],
-   },
-  };
-  
-  export default nextConfig;
+  // images: {
+  //    domains: ['localhost','bit.ly',
+  //       'www.sender.net',
+  //       'hillfresh.eu','cdn-prod.medicalnewstoday.com','t3.ftcdn.net','www.simplyrecipes.com','hips.hearstapps.com'],
+  //  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "***.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost:4000",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
