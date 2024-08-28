f7d0e5fc7ed672e2dd96f039f8635be19cc8ac4e
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-08-28 15:14:32 +0300

    upd footer

--- app/_components/Cards/Footer.tsx
+++ app/_components/Cards/Footer.tsx
@@ -6,13 +6,13 @@ import { ImInstagram } from "react-icons/im";
 const Footer = () => {
   return (
     <footer className="h-[300px] ">
-      <div className="flex justify-between flex-wrap items-center px-24">
+      <div className="flex justify-between flex-wrap items-center gap-5 lg:px-24 px-6">
         <h3 className="text-white text-2xl">
           Subscribe <br />
           to newsletter
         </h3>
 
-        <div className="form flex border-2 lg:w-[40%] mx-auto border-white">
+        <div className="form flex border-2 lg:w-[40%] border-white">
           <input
             type="email"
             name="user-email"
@@ -22,7 +22,7 @@ const Footer = () => {
           <button className="bg-white  p-4">subscribe</button>
         </div>
 
-        <div className="social-squar flex gap-5 lg:w-[25%] h-[150px] bg-slate-500 p-8">
+        <div className="social-squar flex gap-5 lg:w-[25%] w-full h-[150px] bg-slate-500 p-8">
           <Link className="lg:text-2xl" href={`#`}>
             <ImInstagram className="text-yellow-50" />
           </Link>
@@ -35,15 +35,15 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex justify-between flex-wrap items-center px-24 text-white mt-7">
+      <div className="flex justify-between flex-wrap items-center lg:px-24 px-6 text-white mt-7">
         <h3 className="lg:basis-1/4 lg:text-2xl ">Food <br />
         Empire</h3>
 
-        <div className="footer-links flex justify-between items-end lg:basis-9/12 flex-wrap ">
+        <div className="footer-links flex justify-between gap-5 lg:mt-0 mt-6 items-end lg:basis-9/12 flex-wrap ">
           <Link href={'/'}>Best Fruits</Link>
           <Link href={'/'}>Social Media</Link>
           <Link href={'/'}>More for You</Link>
-          <Link href={'/'}>YourShoop</Link>
+          <Link href={'/'}>Your Shope</Link>
           <p>The Copyright 2022 guide covers 14 jurisdictions.</p> 
         </div>
       </div>
